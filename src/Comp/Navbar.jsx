90c8b38fe6035dae72947a0b4fd305fbec8de692
rahul21092001 <rahuldoddi15@gmail.com>
2023-11-30 15:43:35 +0530

    changes

--- src/Comp/Navbar.jsx
+++ src/Comp/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <>
       <nav className="navbar">
-        <ul className="nav"  >
+        <ul className="nav">
           <li className="logo">
             <img src="Main Logo.png" alt="" />
           </li>
-          <li  >
-            <h1 >Cric-Info</h1>
+          <li>
+            <h1 className="heading">Cric-Info</h1>
           </li>
-          <li><button>Signup</button></li>
-          <li><button>Login</button></li>
+          <div className="hdbtn">
+          
+          <li>
+            <NavLink to={"/login"}>
+              <button className="login">Login</button>
+            </NavLink>
+          </li>
+          </div>
         </ul>
       </nav>
     </>
