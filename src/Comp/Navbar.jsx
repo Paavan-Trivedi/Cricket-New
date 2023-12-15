2297c3f6b57b33a3a4a564c4ad7f386501e26436
Panchal Hardik <panchalhardik9551@gmail.com>
2023-12-15 11:38:20 +0530

    ad

--- src/Comp/Navbar.jsx
+++ src/Comp/Navbar.jsx
@@ -1,28 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
-import axios from "axios";
-import { useAuth } from "../context/AuthContext";
 
 export default function Navbar() {
   const navigate = useNavigate();
-  const { token, setToken, setLoggedIn } = useAuth();
 
-  const logoutHandler = () => {
-    axios
-      .post('http://192.168.29.84:8000/accounts/api/logout/', null, {
-        headers: { Authorization: `Token ${token}` },
-      })
-      .then(() => {
-        setToken('');
-        setLoggedIn(false);
-        toast.success('Logout Successfully');
-        navigate("/login")
-      })
-      .catch((error) => {
-        console.error('Logout failed:', error);
-      });
-  };
+  const logoutHandler=()=>{
+    localStorage.removeItem("login")
+    toast.success("logout Successfully")
+    navigate("/login")
+  }
 
   return (
     <>
